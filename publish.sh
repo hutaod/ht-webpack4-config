787e2ad2d740cb3ec69dc2ed9070da7d312332d6
ht1131589588 <1131589588@qq.com>
2019-09-08 18:10:11 +0800

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 npm config get registry #检测仓库镜像
 npm config set registry=http://registry.npmjs.org
-echo '请进行登录相关操作：'
-npm login #登录
+# echo '请进行登录相关操作：'
+# npm login #登录
 echo "-------publishing-------"
 npm publish #发布
 npm config set registry=https://registry.npm.taobao.org # 设置为淘宝镜像
